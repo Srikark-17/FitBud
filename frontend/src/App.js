120910302b64a4294c12f7c6ac2318a6b71ebf1a
Blythe-hub <58117457+Blythe-hub@users.noreply.github.com>
2021-06-26 12:05:38 -0700

    Deployed Backend to Heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const ENDPOINT = "https://fitbud-backend.herokuapp.com/";
+  
   return (
     <div className="App">
       <header className="App-header">
